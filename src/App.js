ce6da1f87be36714eca444a38cce6a208f2055c0
Martin Quevedo <martinqs@live.com.ar>
2024-02-02 10:05:47 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,85 +1,64 @@
-import "./App.css";
-import { useState } from "react";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
 export default function App() {
   return (
-    <div className="App">
-      <TipCalculator />
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
   );
 }
 
-function TipCalculator() {
-  const [bill, setBill] = useState(0);
-  const [percentage1, setPercentage1] = useState(0);
-  const [percentage2, setPercentage2] = useState(0);
-
-  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
-
-  function handleReset() {
-    setBill("");
-    setPercentage1(0);
-    setPercentage2(0);
-  }
-
+function FriendsList() {
+  const friends = initialFriends;
   return (
-    <div>
-      <BillAmount bill={bill} onSetBill={setBill} />
-      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
-        How did you like the service?
-      </SelectPercentage>
-      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
-        How did your friend liked the service?
-      </SelectPercentage>
-      {bill > 0 && (
-        <>
-          <Output bill={bill} tip={tip} />
-          <Reset onReset={handleReset} />{" "}
-        </>
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h2>{friend.name}</h2>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
       )}
-    </div>
-  );
-}
+      {friend.balance > 0 && (
+        <p className="green">
+          Your {friend.name} ows you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-function BillAmount({ bill, onSetBill }) {
-  return (
-    <div>
-      <label>How much was the bill?</label>
-      <input
-        type="text"
-        placeholder="Bill value"
-        value={bill}
-        onChange={(e) => onSetBill(Number(e.target.value))}
-      />
-    </div>
+      <button className="button">Select</button>
+    </li>
   );
-}
-
-function SelectPercentage({ children, percentage, onSelect }) {
-  return (
-    <div>
-      <lavel>{children}</lavel>
-      <select
-        value={percentage}
-        onChange={(e) => onSelect(Number(e.target.value))}
-      >
-        <option value="0">Dissatisfied (0%)</option>
-        <option value="5">It was ok (5%)</option>
-        <option value="15">Pretty good (15%)</option>
-        <option value="20">Amazing (20%)</option>
-      </select>
-    </div>
-  );
-}
-
-function Output({ bill, tip }) {
-  return (
-    <h2>
-      You pay {bill + tip} (${bill} + ${tip})
-    </h2>
-  );
-}
-
-function Reset({ onReset }) {
-  return <button onClick={onReset}>Reset</button>;
 }
